2003761b6ce6bd9c7c090772f6edc5e0ee46528c
Alena <alena.povar@gmail.com>
2022-01-28 16:37:31 +0300

    Add withdraw

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -2,13 +2,24 @@ package org.xpdojo.bank;
 
 public class Account {
 
-    private double deposit;
+    private double amount;
     public Account() {
-        deposit = 0;
+        amount = 0;
     }
 
-    public double getDeposit(){
-        return deposit;
+    public double checkAccount(){
+        return amount;
     }
 
+    public void deposit(double amount) {
+
+        this.amount = this.amount + amount;
+    }
+
+    public void withdraw(double amount){
+        if(amount > this.amount)
+            throw new IllegalArgumentException("Not enough money");
+
+        this.amount = this.amount - amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,6 +1,5 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,7 +9,33 @@ public class AccountTest {
     @Test
     public void createAccount() {
         Account account = new Account();
-        assertThat(account.getDeposit()).isEqualTo(0);
+        assertThat(account.checkAccount()).isEqualTo(0);
+    }
+
+    @Test
+    public void addAmountToAccount() {
+
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.checkAccount()).isEqualTo(30);
+
+    }
+
+    @Test
+    public void withdrawAccount() {
+        Account account = new Account();
+        account.deposit(10);
+        account.withdraw(5);
+        assertThat(account.checkAccount()).isEqualTo(5);
+
+    }
+
+    @Test
+    public void withdrawWIthErrorAccount() {
+        Account account = new Account();
+        account.deposit(10);
+        account.withdraw(20);
     }
 
 }
